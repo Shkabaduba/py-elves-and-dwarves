6f98bd9f61ae7815e056308d7c9a0e55bd8c42ab
Oleksandr Shkabbura <shkabura007@gmail.com>
2026-07-02 23:07:10 +0300

    Elves Dwarf

---
+++ app/players/elves/elf.py
@@ -0,0 +1,13 @@
+from ..player import Player
+
+
+class Elf(Player):
+    def __init__(self, nickname: str, musical_instrument: str):
+        self._musical_instrument = musical_instrument
+        super().__init__(nickname)
+
+    def play_elf_song(self):
+        print (f"{self.nickname} is playing a song on the {self._musical_instrument}")
+
+    def declared(self):
+        pass
---
+++ app/players/elves/elf_ranger.py
@@ -0,0 +1,16 @@
+from .elf import Elf
+
+
+class ElfRanger(Elf):
+    def __init__(self, nickname: str, musical_instrument: str, bow_level: int):
+        self._bow_level = bow_level
+        super().__init__(nickname, musical_instrument)
+
+    def player_info(self):
+        return f"Elf ranger {self.nickname}. {self.nickname} has bow of the {self._bow_level} level"
+
+    def get_rating(self):
+        return self._bow_level * 3
+
+    def declared(self):
+        pass
